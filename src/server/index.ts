0048865adbf5d792c66e028b14518b8ae7c63c9e
Gan Shuai <sgan@thoughworks.com>
2019-10-01 09:29:02 +0800

    save

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,15 +13,31 @@ app.get('/', (req, res) => {
 app.get('/pages/:path', (req, res) => {
     const html = fs.readFileSync(`${pagesPath}/${req.params.path}`).toString()
 
-    // html.replace('</body>', "")
-
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    setHeader(res, HttpHeaderContentTypeValue.Html)
     res.send(html)
 })
 
+app.get('/feature-test', (req, res) => {
+    setHeader(res, HttpHeaderContentTypeValue.Html)
+})
+
 app.get('/dist/:path', (req, res) => {
-    res.setHeader('Content-Type', 'application/javascript; charset=utf-8')
+    setHeader(res, HttpHeaderContentTypeValue.Js)
     res.send(fs.readFileSync(`${distPath}/${req.params.path}`))
 })
 
 app.listen(81)
+
+
+enum HttpHeaderKey {
+    ContentType = 'Content-Type'
+}
+
+enum HttpHeaderContentTypeValue {
+    Html = 'text/html; charset=utf-8',
+    Js = 'application/javascript; charset=utf-8'
+}
+
+function setHeader(res: express.Response, type: HttpHeaderContentTypeValue) {
+    res.setHeader(HttpHeaderKey.ContentType, type)
+}
